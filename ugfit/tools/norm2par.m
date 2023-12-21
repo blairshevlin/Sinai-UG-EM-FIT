3022bfc4463b0221a5367020a99fd3ad7c24403b
blairshevlin <49033862+blairshevlin@users.noreply.github.com>
2023-12-21 13:49:24 -0500

    More 2cond scripts

--- ugfit/tools/norm2par.m
+++ ugfit/tools/norm2par.m
@@ -100,20 +100,20 @@ elseif strcmp(modelID, 'ms_UG3_etaf_f0f_adaptiveNorm_2cond')
     qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2alpha(qin(:,3))  norm2delta(qin(:,4))  norm2delta(qin(:,5))];
     qbounds = [0 1; 0 15; 0 1; -2 2; -2 2]';
 elseif strcmp(modelID, 'ms_UG0_etaf_f0f_noNorm_2cond')
-    if size(qin,2)~=3, disp('ERROR'); keyboard; end
+    if size(qin,2)~=2, disp('ERROR'); keyboard; end
     qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2))];
     qbounds = [0 1; 0 15]';
 elseif strcmp(modelID, 'ms_UG1_etaf_f0f_noNorm_2cond')
-    if size(qin,2)~=5, disp('ERROR'); keyboard; end
-    qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2delta(qin(:,4))  norm2delta(qin(:,5))];
+    if size(qin,2)~=4, disp('ERROR'); keyboard; end
+    qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2delta(qin(:,3))  norm2delta(qin(:,4))];
     qbounds = [0 1; 0 15; -2 2; -2 2]';
 elseif strcmp(modelID, 'ms_UG2_etaf_f0f_noNorm_2cond')
-    if size(qin,2)~=5, disp('ERROR'); keyboard; end
-    qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2delta(qin(:,4))  norm2delta(qin(:,5))];
+    if size(qin,2)~=4, disp('ERROR'); keyboard; end
+    qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2delta(qin(:,3))  norm2delta(qin(:,4))];
     qbounds = [0 1; 0 15; -2 2; -2 2]';
 elseif strcmp(modelID, 'ms_UG3_etaf_f0f_noNorm_2cond')
-    if size(qin,2)~=5, disp('ERROR'); keyboard; end
-    qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2delta(qin(:,4))  norm2delta(qin(:,5))];
+    if size(qin,2)~=4, disp('ERROR'); keyboard; end
+    qout = [norm2alpha(qin(:,1)) norm2beta(qin(:,2)) norm2delta(qin(:,3))  norm2delta(qin(:,4))];
     qbounds = [0 1; 0 15; -2 2; -2 2]';
    
 end
